3d6491137d370eaa64ca10feb2bb8b614d6b3e7d
siwydb <maciekbann@gmail.com>
2015-02-24 23:18:50 +0100

    add services

--- src/main/java/pl/banachowski/psoir/controller/FilterController.java
+++ src/main/java/pl/banachowski/psoir/controller/FilterController.java
@@ -1,7 +1,10 @@
 package pl.banachowski.psoir.controller;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
+import javafx.scene.transform.Rotate;
 import org.imgscalr.Scalr;
 import org.springframework.stereotype.Controller;
 
@@ -10,5 +13,14 @@ public class FilterController {
 	public BufferedImage scale(BufferedImage buffImg) {
 		return Scalr.resize(buffImg, Scalr.Method.SPEED, 100);
 	}
+
+    public BufferedImage rotate (BufferedImage bufferedImage) {
+
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(3.14, bufferedImage.getWidth()/2, bufferedImage.getHeight()/2);
+        AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+        bufferedImage = op.filter(bufferedImage, null);
+        return bufferedImage;
+    }
 
 }
