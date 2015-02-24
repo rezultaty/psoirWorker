81495fa3fbf2bc4ed231328ae912033934212089
siwydb <maciekbann@gmail.com>
2015-02-25 00:21:42 +0100

    fix service

--- src/main/java/pl/banachowski/psoir/controller/FilterController.java
+++ src/main/java/pl/banachowski/psoir/controller/FilterController.java
@@ -4,7 +4,6 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
-import javafx.scene.transform.Rotate;
 import org.imgscalr.Scalr;
 import org.springframework.stereotype.Controller;
 
